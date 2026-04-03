49d6ed488864d521c9548da2dddb7ca871117093
Freezer-tech <54477347+Freezer-tech@users.noreply.github.com>
2026-04-03 21:59:13 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,20 @@
+const vendite = {
+  "Subito": [
+    { nome: "Samsung Galaxy S5", persona: "Giuseppe", link: "https://www.subito.it/" },
+    { nome: "Samsung Gear VR", persona: "Gino", link: "https://www.subito.it/" }
+  ],
+
+  "Vinted": [
+    { nome: "Jeans uomo", persona: "Luca", link: "#" },
+    { nome: "Apple Pencil USB-C", persona: "Andrea", link: "#" }
+  ],
+
+  "Facebook": [
+    { nome: "Playstation 2", persona: "Vincenzo", link: "#" }
+  ]
+};
+
+const acquisti = [
+  { nome: "Yamaha P35", persona: "Luca" },
+  { nome: "Galaxy S21", persona: "Marco" }
+];
